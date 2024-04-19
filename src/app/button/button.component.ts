11c1a6c56c76693b72bdae362cb3c41fafc627ed
sebastian tovar <sebastiantovarchavez304@gmail.com>
2024-04-19 13:50:16 -0500

    :tada: inicio de proyecto

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({ //decorador
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
