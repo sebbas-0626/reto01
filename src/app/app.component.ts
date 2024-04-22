c58a5c7e771df6ad27e5793179fe2adcdb27167b
sebastian tovar <sebastiantovarchavez304@gmail.com>
2024-04-22 18:38:53 -0500

    :sparkles: Event binding ó (enlace de eventos ) Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  cities = ['Barcelona','Madril','colombia']
+// ciclo de vida de los componentes
+export class AppComponent implements OnChanges, OnInit, OnDestroy {
+  cities = ['Barcelona', 'Madril', 'colombia']
   name!: string;
+  selection!: string;
   title = 'reto01';
   url = 'https://storage-asset.msi.com/global/picture/news/2023/nb/mercedes-20230531-1.jpg'
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('change', changes);
+  }
+  ngOnInit(): void {
+    console.log('Oninit');
+  }
+  ngOnDestroy(): void {
+    console.log('destroy');
+  }
+  oncityCliked(city: string) {
+    console.log('evnt=>', city);
+    this.selection = city;
+  }
+  onClear(): void {
+    this.selection = '';
+  }
 }
 
 // logica
